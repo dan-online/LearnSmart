54c7ac1186eec456fa600d5c5f33772f1ee87687
MayorChano <Dannyrwilson@me.com>
2019-05-20 08:36:08 +0200

    fix index.php

--- index.php
+++ index.php
@@ -28,10 +28,37 @@ unset($_SESSION['registered_success']);
 
  <main role="main">
 
-
-<div class="container main">
-
-</div>  
+ <div class="container main">
+  <!-- Example row of columns -->
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="/1819dw/LearnSmart/assets/images/office.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Orest Brykowytch</h5>
+          <a href="https://orestbrykowytch.github.io" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="/1819dw/LearnSmart/assets/images/globe.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Daniel Wilson</h5>
+          <a href="https://orestbrykowytch.github.io" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="/1819dw/LearnSmart/assets/images/arrow.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Adam Whitmore</h5>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </main>
 
