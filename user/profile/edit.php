9ac6f55a4b818201be84b3cb8ea1e7f5d360cb5c
Orest Brykowytch <orestsbryk@gmail.com>
2019-06-04 11:22:45 +0200

    still not working, but this is an improvement
    lmao

--- user/profile/edit.php
+++ user/profile/edit.php
@@ -11,7 +11,10 @@ $ddl = $_POST['ddl'];
 $scripted = $_POST['scripted'];
 $level = $_POST['level'];
 
-$result = mysqli_query($connect, "UPDATE LearnSmart SET level = '$level', compiled = '$compiled', interpreted = '$interpreted', ddl = '$ddl', scripted = '$scripted' WHERE unique_id = '$unique_id_of_logged_in_user';");
-header('location:../../index.php');
+$result = mysqli_query($connect, "INSERT INTO `LearnSmart` (`level`, `compiled`, `interpreted`, `ddl`, `scripted`) VALUES (`$level`, `$compiled`, `$interpreted`, `$ddl`, `$scripted`) WHERE unique_id = '$unique_id_of_logged_in_user';");
+
+# include("../../main/footer.php");
+header('location:../../user/profile/index.php');
+
 
 ?>
