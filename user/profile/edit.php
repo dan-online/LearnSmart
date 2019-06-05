145712a9c018dd3ebf0b53d122e4356e2f8fc616
MayorChano <dan@dancodes.online>
2019-06-05 09:47:31 +0200

    working update you nonce

--- user/profile/edit.php
+++ user/profile/edit.php
@@ -3,15 +3,22 @@ session_start();
 include("../../main/database/database_inc.php");
 include("../../main/database/access_control.php");
 
-$password = $_POST['password'];
-$username = $_POST['username'];
 $compiled = $_POST['compiled'];
 $interpreted = $_POST['interpreted'];
 $ddl = $_POST['ddl'];
 $scripted = $_POST['scripted'];
 $level = $_POST['level'];
 
+
+
+
 $result = mysqli_query($connect, "UPDATE LearnSmart SET level = '$level', compiled = '$compiled', interpreted = '$interpreted', ddl = '$ddl', scripted = '$scripted' WHERE unique_id = '$unique_id_of_logged_in_user';");
+if($result) {
+    echo $result;
+}
+echo '<pre><code>';
+print_r(get_defined_vars()); 
+echo '</pre></code>';
 header('location:../../index.php');
 
 ?>
